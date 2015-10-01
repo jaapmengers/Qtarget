49d59d19cadf28d9d89a1a8993e5a18e1ff4a181
Jaap Mengers <jaapm@q42.nl>
2015-10-01 21:07:11 +0200

    Puts the communication with the target in an actor and uses the ask pattern to turn triggering the target and receiving its result into a future

--- app/controllers/BrowserTarget.scala
+++ app/controllers/BrowserTarget.scala
@@ -1,74 +1,100 @@
 package controllers
 
 import akka.actor.Actor.Receive
+import akka.pattern.{ ask }
 import akka.actor.{Actor, Props, ActorRef}
+import akka.util.Timeout
 import play.api.libs.concurrent.Akka
 import play.api.libs.json.{Json, JsValue}
+import play.api.libs.ws.WS
 import play.api.mvc._
 import play.api.mvc.Results._
 import play.mvc.Controller
 import play.api.Play.current
+import scala.concurrent.duration._
+import scala.concurrent.ExecutionContext.Implicits.global
+
+
+object ViewModelFormats {
+  import play.api.libs.json.Json
+
+  implicit val vmHitFormat = Json.format[Hit]
+}
+
+case object Up
+trait Result
+case class Hit(time: Long) extends Result
+case object Miss extends Result
+
+case class ForwardedMessage(actorName: String, result: Result)
 
 object BrowserTarget extends Controller {
 
-  val oa = Akka.system.actorOf(Props[OrchestrationActor])
+  import ViewModelFormats._
+
+  val targets = Map(
+    "een" ->  Akka.system.actorOf(TargetActor.props("77Voox93zEaE"))
+  )
+
+  val ca = Akka.system.actorOf(CommunicationActor.props(targets))
+
+  implicit val timeout = Timeout(20 seconds)
+
 
   def index = Action { implicit request =>
     Ok(views.html.index())
   }
 
-  def socket = WebSocket.acceptWithActor[JsValue, JsValue] { request => out =>
-    BrowserTargetActor.props(out, oa)
-  }
-}
+  def up = Action.async { implicit request =>
+    val resultF = (targets.head._2 ? Up).mapTo[Result]
 
-object BrowserTargetActor {
-  def props(out: ActorRef, orchestrationActor: ActorRef) = Props(new BrowserTargetActor(out, orchestrationActor))
-}
-
-case object Register
-
-class BrowserTargetActor(out: ActorRef, orchestrationActor: ActorRef) extends Actor {
-
-  override def preStart() = {
-    orchestrationActor ! Register
-  }
-
-  def receive = {
-    case msg: JsValue => {
-      out !  Json.obj("text" -> s"I received your message: ${(msg \ "text").as[String]}")
+    for {
+      result <- resultF
+    } yield {
+      println(s"Result was $result")
+      Redirect(routes.BrowserTarget.index)
     }
   }
+
+  def hit = Action(BodyParsers.parse.json) { implicit request =>
+    val v = request.body.as[Hit]
+
+    ca ! ForwardedMessage("een", v)
+    Ok
+  }
+
+  def miss = Action { implicit request =>
+    ca ! ForwardedMessage("een", Miss)
+    Ok
+  }
 }
 
-class OrchestrationActor extends Actor {
-  import context._
 
-  var targets = Map.empty[String, ActorRef]
-
-  def instantiate(socket: ActorRef) = {
-
-    val targetNames = Set("een", "twee", "drie")
-    targets = targetNames.map(x => x -> context.actorOf(TargetActor.props(x))).toMap
-
-    socket ! Json.obj("targets" -> targetNames)
-
-    become(connected)
-  }
-
+class CommunicationActor(targets: Map[String, ActorRef]) extends Actor {
   override def receive: Receive = {
-    case Register =>
-      instantiate(sender())
+    case ForwardedMessage(actorName, result) =>
+      for {
+        target <- targets.get(actorName)
+      } yield target ! result
   }
+}
 
-  def connected: Receive = {
-    case _ => //
-  }
+object CommunicationActor {
+  def props(targets: Map[String, ActorRef]) = Props(new CommunicationActor(targets))
 }
 
 class TargetActor(targetid: String) extends Actor {
+
+  var originalSender: Option[ActorRef] = None
+
   override def receive: Receive = {
-    case _ => //
+    case Up =>
+      originalSender = Some(sender())
+      for { x <-  WS.url(s"https://agent.electricimp.com/$targetid/up?text=&user_name=jaapm").get() } yield ()
+    case result: Result => originalSender match {
+      case Some(s: ActorRef) => s ! result
+      case _ => //
+    }
   }
 }
 
